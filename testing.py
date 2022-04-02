8956ae0b19789ff00d0115268861a6aa4a089e04
Jarrod McCarthy <jarrodmccarthy12@gmail.com>
2022-04-03 09:36:35 +1000

    Jarrod

--- testing.py
+++ testing.py
@@ -1,14 +1,29 @@
 from pymycobot import MyCobot
+import time
 
 mc = MyCobot("/dev/ttyAMA0", 1000000)
 
-
+startposition = [0, -15, 0, 0, 0, 0]
+bin1 = [0, -15, 0, 0, 0, 0]
+bin2 = [0, -15, 0, 0, 0, 0]
+bin3 = [0, -15, 0, 0, 0, 0]
+bin4 = [0, -15, 0, 0, 0, 0]
+shortsleep = 0.1
 
 def send_to_start(mc):
-    mc.send_angles([0, -100, 0, 0, 0, 0], 50)
+    mc.send_angles(startposition, 50)
+    time.sleep(shortsleep)
     return
 
-print(mc.get_angles())
-print(mc.get_coords())
+def send_to_bin(mc, binpos):
+    mc.send_angles(binpos, 50)
+    time.sleep(shortsleep)
+    return 
+
+def grab_item():
+    pass
+
+
 send_to_start(mc)
-print(mc.get_angles())
+
+
