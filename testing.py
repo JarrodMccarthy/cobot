0e5b3fbcdbe8d1e5b065b90390a7b0a802ce7e96
Jarrod McCarthy <jarrodmccarthy12@gmail.com>
2022-04-03 09:29:53 +1000

    Jarrod

--- testing.py
+++ testing.py
@@ -2,6 +2,13 @@ from pymycobot import MyCobot
 
 mc = MyCobot("/dev/ttyAMA0", 1000000)
 
-print(mc.get_angles())
 
+
+def send_to_start(mc):
+    mc.send_angles([0, -100, 0, 0, 0, 0], 50)
+    return
+
+print(mc.get_angles())
 print(mc.get_coords())
+send_to_start(mc)
+print(mc.get_angles())
