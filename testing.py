34d880f10d383cef0dd8490ac348349073c8b185
wuchangji <changji.wu@elephantrobotics.com>
2022-04-03 07:52:09 +0800

    Jarrod: Defined 3 bins

--- testing.py
+++ testing.py
@@ -4,11 +4,11 @@ import time
 mc = MyCobot("/dev/ttyAMA0", 1000000)
 
 startposition = [0, -15, 0, 0, 0, 0]
-bin1 = [0, -15, 0, 0, 0, 0]
-bin2 = [0, -15, 0, 0, 0, 0]
-bin3 = [0, -15, 0, 0, 0, 0]
+bin1 = [115, 10, -100, 5, 0, 0]
+bin2 = [80, -10, -80, 5, 0, 0]
+bin3 = [50, -45, -37, 10, 0, 0]
 bin4 = [0, -15, 0, 0, 0, 0]
-shortsleep = 0.1
+shortsleep = 0.5
 
 def send_to_start(mc):
     mc.send_angles(startposition, 50)
@@ -25,5 +25,6 @@ def grab_item():
 
 
 send_to_start(mc)
+send_to_bin(mc, bin3)
 
 
